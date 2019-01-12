307268e2624cc3c8cc846832a8951c9923c1ae7f
Slu True <slu.true@gmail.com>
2019-01-11 19:34:08 -0800

    Added modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
 
 const base64Img = require('base64-img');
 
@@ -8,11 +10,26 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      
+
     }
 
-    this.sendAPIRequest = () => {
+    this.sendAPIRequest = (imageURI) => {
+      const image64 = base64Img.base64Sync(imageURI);
 
+      client
+        .labelDetection('./resources/wakeupcat.jpg')
+        .then(results => {
+          const labels = results[0].labelAnnotations;
+ 
+          console.log('Labels:');
+          labels.forEach(label => console.log(label.description));
+
+          //handle displaying labels.
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+        //handle error here
+      });
     }
 
     this.handleAnnotations = () => {
